e84542ee09a9966818ae838352e9115daec6eb20
Nathan aka SoCalGuitarist <114831821+SanDiegoDude@users.noreply.github.com>
2025-04-09 21:40:49 -0700

    Update pipeline_hidream_image.py
    wipe out useless warning message about CLIP truncations

--- hi_diffusers/pipelines/hidream_image/pipeline_hidream_image.py
+++ hi_diffusers/pipelines/hidream_image/pipeline_hidream_image.py
@@ -216,13 +216,6 @@ class HiDreamImagePipeline(DiffusionPipeline, FromSingleFileMixin):
         )
 
         text_input_ids = text_inputs.input_ids
-        untruncated_ids = tokenizer(prompt, padding="longest", return_tensors="pt").input_ids
-        if untruncated_ids.shape[-1] >= text_input_ids.shape[-1] and not torch.equal(text_input_ids, untruncated_ids):
-            removed_text = tokenizer.batch_decode(untruncated_ids[:, 218 - 1 : -1])
-            logger.warning(
-                "The following part of your input was truncated because CLIP can only handle sequences up to"
-                f" {218} tokens: {removed_text}"
-            )
         prompt_embeds = text_encoder(text_input_ids.to(device), output_hidden_states=True)
 
         # Use pooled output of CLIPTextModel
