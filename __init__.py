7f3d4bbddbfca35aa84e9c386a94a1ce846bcc57
Nathan aka SoCalGuitarist <114831821+SanDiegoDude@users.noreply.github.com>
2025-04-09 22:02:50 -0700

    Update __init__.py
    fixed import

--- __init__.py
+++ __init__.py
@@ -1,14 +1,12 @@
-from .hidreamsampler import HiDreamSampler
+from .hidreamsampler import HiDreamSampler, HiDreamSamplerAdvanced
 
 NODE_CLASS_MAPPINGS = {
     "HiDreamSampler": HiDreamSampler,
     "HiDreamSamplerAdvanced": HiDreamSamplerAdvanced
 }
-
 NODE_DISPLAY_NAME_MAPPINGS = {
     "HiDreamSampler": "HiDream Sampler",
     "HiDreamSamplerAdvanced": "HiDream Sampler (Advanced)"
 }
-
 WEB_DIRECTORY = "./web"
 __all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS", "WEB_DIRECTORY"]
